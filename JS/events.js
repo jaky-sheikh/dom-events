cc538caa29e078afb50dea3791bcc24c94f79020
jaky-sheikh <jaky@eu4m.eu>
2025-06-25 12:03:46 +0600

    3rd syster to add an events

--- JS/events.js
+++ JS/events.js
@@ -3,3 +3,9 @@ console.log('This is separate js file');
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
+
+const blueButton = document.getElementById('make-blue');
+blueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
